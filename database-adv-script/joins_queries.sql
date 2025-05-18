e5dbdf0cbe68837d11f73def6ac2b7c2202890cf
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-05-18 12:46:52 +0000

    include ORDER BY

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -12,16 +12,10 @@ INNER JOIN
     users ON bookings.user_id = users.id;
 
 -- LEFT JOIN: Retrieve all properties and their reviews, including properties with no reviews
-SELECT 
-    properties.id AS property_id,
-    properties.title,
-    reviews.id AS review_id,
-    reviews.rating,
-    reviews.comment
-FROM 
-    properties
-LEFT JOIN 
-    reviews ON properties.id = reviews.property_id;
+SELECT properties.id AS property_id, properties.title, reviews.rating, reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
 
 -- FULL OUTER JOIN: Retrieve all users and all bookings, even if there's no link between them
 SELECT 
