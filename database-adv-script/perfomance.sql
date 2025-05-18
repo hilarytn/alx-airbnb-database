8c820989e25133245cacffcb000cbe2a7db5ca5b
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-05-18 20:57:07 +0000

    Redo performance sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,17 +1,15 @@
+EXPLAIN ANALYZE
 SELECT 
     b.id AS booking_id,
     b.booking_date,
     b.status,
-    
     u.id AS user_id,
     u.name AS user_name,
     u.email AS user_email,
-    
     p.id AS property_id,
     p.title AS property_title,
     p.location AS property_location,
     p.price AS property_price,
-    
     pay.id AS payment_id,
     pay.amount AS payment_amount,
     pay.payment_date
@@ -20,4 +18,5 @@ JOIN User u ON b.user_id = u.id
 JOIN Property p ON b.property_id = p.id
 LEFT JOIN Payment pay ON pay.booking_id = b.id
 WHERE b.status = 'confirmed'
+  AND p.price < 500
 ORDER BY b.booking_date DESC;
