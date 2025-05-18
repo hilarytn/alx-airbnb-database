87c1053e0389be858a89ae99e055055c6f5db01d
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-05-18 20:20:55 +0000

    include row number

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,8 +3,11 @@ SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
 
--- 2. Rank properties based on the total number of bookings using RANK()
-SELECT property_id, COUNT(*) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM bookings
-GROUP BY property_id;
+-- Rank properties based on the total number of bookings using RANK()
+SELECT property_id, total_bookings,
+       RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT property_id, COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+) AS booking_counts;
